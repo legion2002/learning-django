4cc8d96634f818d6427b3015a660239c34577d88
Tanishk Goyal <goyaltanishk02@gmail.com>
2022-01-19 16:27:54 +0530

    Templates and Views coming in

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# This is a request handler
+
+
+def trial_func(request):
+    return HttpResponse("Wazzzupppp")
